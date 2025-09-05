3bbb65bcc0133c812dd2bba7d6764d862b4d8c3e
DHARKSYD <davidbenjaminorendu@gmail.com>
2025-09-05 14:39:10 +0100

    Fix: Successful build, ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import { Toaster } from '@/components/ui/toaster';
-//import { useToast } from '@/components/ui/use-toast';
+import { Toaster } from '@/components/ui/toaster';
 import Header from '@/components/Header';
 import HomePage from '@/pages/HomePage';
 import SearchPage from '@/pages/SearchPage';
@@ -38,7 +37,7 @@ function App() {
             <Route path="/footer-info" element={<FooterInfoPage />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
-          
+          <Toaster />
           <Footer />
         </div>
       </Router>
@@ -47,4 +46,3 @@ function App() {
 }
 
 export default App;
-//<Toaster />
